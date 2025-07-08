f1f5a8c143061a196f209f79a9f660409b91da07
Mayur244 <mayurghodaskar44@gmail.com>
2025-07-08 17:06:12 +0530

    setup redux store

---
+++ src/components/Login.jsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("salman@example.com");
+  const [password, setPassword] = useState("Salman@123");
+  const [error, setError] = useState("");
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data))
+      navigate("/")
+
+    } catch (error) {
+      if (error.response) {
+        // console.error("Error :", error);
+        setError(error.response.data);
+      }
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-20">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <label className="pl-2">
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+                className="input"
+              />
+            </fieldset>
+          </label>
+          <label className="pl-2">
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input"
+              />
+            </fieldset>
+          </label>
+          <div>
+            <p className="text-red-300 pl-2 text-[12px]">{error}</p>
+          </div>
+          <div className="card-actions justify-center py-4">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
